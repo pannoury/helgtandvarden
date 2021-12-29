d90ab3365d21ab73d9604244e0600b9c439d5c7a
Rendex-sweden <84630659+Rendex-sweden@users.noreply.github.com>
2021-12-29 10:53:45 +0100

    init

---
+++ public_html/JS/main.js
@@ -0,0 +1,50 @@
+window.onload = () => {
+    navbarComponents.navbarScrollEffect()
+    navbarComponents.addEventListeners()
+}
+
+
+const navbarComponents = {
+    addEventListeners: () => {
+        window.addEventListener('scroll', function(){
+            navbarComponents.navbarScrollEffect();
+        })
+        
+        document.getElementById('nav-button').addEventListener('click', function(){
+            navbarComponents.navbarMobileHamburger(document.getElementById('nav-button').getAttribute('aria-label'));
+        })
+    },
+    navbarScrollEffect: () => {
+        var navbar = document.getElementsByTagName('nav')[0];
+        if(window.innerWidth > 875){
+            if(window.pageYOffset >= 1){
+                navbar.style.backgroundColor = "#19253c"
+                navbar.classList.add('filled')
+            }
+            else{
+                navbar.style.backgroundColor = "";
+                navbar.classList.remove('filled')
+            }
+        } else{
+            navbar.style.backgroundColor = "#19253c"
+        }
+    },
+    navbarMobileHamburger: (attribute) => {
+        var navbar = document.querySelector('.nav-desktop');
+        if(attribute === "hidden"){
+            document.getElementById('nav-button').setAttribute('aria-label', 'toggled')
+            navbar.style.cssText = `width: 100vw;`
+            document.getElementById('nav-button').innerHTML = 
+            `
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M18.3 5.71c-.39-.39-1.02-.39-1.41 0L12 10.59 7.11 5.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41L10.59 12 5.7 16.89c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0L12 13.41l4.89 4.89c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4z"></path></svg>
+            `
+        } else{
+            document.getElementById('nav-button').setAttribute('aria-label', 'hidden')
+            navbar.style.cssText = `width: 0vw`
+            document.getElementById('nav-button').innerHTML = 
+            `
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M4 18h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zm0-5h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zM3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1z"></path></svg>
+            `
+        }
+    }
+}
