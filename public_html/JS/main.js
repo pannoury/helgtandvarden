204471c84be022ea7d1c648c382d7772b2209230
Rendex-sweden <84630659+Rendex-sweden@users.noreply.github.com>
2021-12-29 17:15:26 +0100

    update 1.0

--- public_html/JS/main.js
+++ public_html/JS/main.js
@@ -1,6 +1,8 @@
 window.onload = () => {
     navbarComponents.navbarScrollEffect()
     navbarComponents.addEventListeners()
+    sectionScrollAnimations();
+    appearEffect()
 }
 
 
@@ -48,3 +50,51 @@ const navbarComponents = {
         }
     }
 }
+
+
+function appearEffect(){
+    var loadElements = document.querySelectorAll('.load')
+    for(let i=0; i<loadElements.length; i++){
+        loadElements[i].style.cssText = "opacity: 1;"
+    }
+}
+
+
+function sectionScrollAnimations(){
+    const faders = document.querySelectorAll('.fade-in');
+    const appearOptions = {
+        threshold: 0,
+        rootMargin: "0px 0px -250px 0px"
+    };
+    const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll){
+        entries.forEach(entry => {
+            if(!entry.isIntersecting){
+                return;
+            }
+            else{
+                entry.target.classList.add("appear")
+                appearOnScroll.unobserve(entry.target);
+            }
+        });
+    }, appearOptions)
+    
+    faders.forEach(fader => {
+        appearOnScroll.observe(fader);
+    })
+
+    /*
+    sliders.forEach(slider =>{
+        appearOnScroll.observe(slider);
+    })
+    popup.forEach(popups => {
+        appearOnScroll.observe(popups);
+    })
+    featuredProjects.forEach(popups => {
+        appearOnScroll.observe(popups)
+    })
+    headers.forEach(popups => {
+        appearOnScroll.observe(popups)
+    })
+    */
+}
+
